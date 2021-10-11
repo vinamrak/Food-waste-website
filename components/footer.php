4ed48de6a093d560b02e5999b6a1fee0c1c2929f
Vinamra Khoria <Vinamrakhoria@gmail.com>
2021-10-11 13:12:07 +0530

    pushing files

---
+++ components/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="p-3 bg-dark text-center text-white">
+    <div class="col"> Developed by <u>Vinamra Khoria</u> & <u>Devansh Baid</u> & <u>Amit Krishna</u> </div>
+    </div>
+</footer>
